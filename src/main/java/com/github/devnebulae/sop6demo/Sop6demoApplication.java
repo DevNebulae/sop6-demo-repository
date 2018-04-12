2606b842568838c5d0cb67d700cee710747ab88f
DevNebulae <gamerkeeperson+github@gmail.com>
2018-04-12 13:36:30 +0200

    Add REST endpoint

--- src/main/java/com/github/devnebulae/sop6demo/Sop6demoApplication.java
+++ src/main/java/com/github/devnebulae/sop6demo/Sop6demoApplication.java
@@ -2,6 +2,16 @@ package com.github.devnebulae.sop6demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class Controller {
+	@GetMapping
+	public String greeting() {
+		return "Hello!";
+	}
+}
 
 @SpringBootApplication
 public class Sop6demoApplication {
