3bd28a57f90cb24ab9eb4b1f54c4410b3fb75df8
DevNebulae <gamerkeeperson+github@gmail.com>
2018-04-12 12:47:47 +0200

    Add Spring Boot environment

---
+++ src/main/java/com/github/devnebulae/sop6demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.github.devnebulae.sop6demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Sop6demoApplication.class);
+	}
+
+}
---
+++ src/main/java/com/github/devnebulae/sop6demo/Sop6demoApplication.java
@@ -0,0 +1,12 @@
+package com.github.devnebulae.sop6demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sop6demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sop6demoApplication.class, args);
+	}
+}
